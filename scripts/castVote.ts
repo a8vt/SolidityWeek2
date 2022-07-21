45c0ef1582de72a268907157038dd70fb825282f
a8vt <105461705+a8vt@users.noreply.github.com>
2022-07-22 00:00:39 +0200

    Update castVote.ts

--- scripts/castVote.ts
+++ scripts/castVote.ts
@@ -10,31 +10,31 @@ const EXPOSED_KEY =
   "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
 
 
-  function setupProvider() {
-    const infuraOptions = process.env.INFURA_API_KEY
-      ? process.env.INFURA_API_SECRET
-        ? {
-            projectId: process.env.INFURA_API_KEY,
-            projectSecret: process.env.INFURA_API_SECRET,
-          }
-        : process.env.INFURA_API_KEY
-      : "";
-    const options = {
-      alchemy: process.env.ALCHEMY_API_KEY,
-      infura: infuraOptions,
-      etherscan : process.env.ETHERSCAN_API_KEY,
-    };
-    const provider = ethers.providers.getDefaultProvider("ropsten", options);
-    return provider;
-  }
+function setupProvider() {
+  const infuraOptions = process.env.INFURA_API_KEY
+    ? process.env.INFURA_API_SECRET
+      ? {
+        projectId: process.env.INFURA_API_KEY,
+        projectSecret: process.env.INFURA_API_SECRET,
+      }
+      : process.env.INFURA_API_KEY
+    : "";
+  const options = {
+    alchemy: process.env.ALCHEMY_API_KEY,
+    infura: infuraOptions,
+    etherscan: process.env.ETHERSCAN_API_KEY,
+  };
+  const provider = ethers.providers.getDefaultProvider("ropsten", options);
+  return provider;
+}
 
-  
+
 async function main() {
   const wallet =
     process.env.MNEMONIC && process.env.MNEMONIC.length > 0
       ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
       : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
-      //: new ethers.Wallet(process.env.PRIVATE_KEY_2 ?? EXPOSED_KEY);
+  //: new ethers.Wallet(process.env.PRIVATE_KEY_2 ?? EXPOSED_KEY);
   console.log(`Using address ${wallet.address}`);
   const provider = setupProvider();
   // const provider = ethers.providers.getDefaultProvider("ropsten");
@@ -62,15 +62,30 @@ async function main() {
   ) as CustomBallot;
 
 
+  function setListeners(
+    ballotContract: ethers.Contract,
+    provider: ethers.providers.BaseProvider
+  ) {
+    console.log("Setting listeners on");
+    const eventFilter = ballotContract.filters.Voted();
+    provider.on(eventFilter, (log: any) => {
+      console.log("Voted");
+      console.log({ log });
+    });
+  }
+
+  setListeners(customBallotContract, provider);
+
+
   if (proposal === "")
     throw new Error("The proposal is empty");
 
-if (amount === "")
-throw new Error ("The amount is empty");
+  if (amount === "")
+    throw new Error("The amount is empty");
 
   console.log("Casting a vote");
   //wrong (compared to test scripts): const transaction = await customBallotContract.vote(proposal, ethers.utils.parseEther(parseInt(amount).toFixed(18)));
-  const transaction = await customBallotContract.vote(proposal,amount);
+  const transaction = await customBallotContract.vote(proposal, amount);
   console.log("Awaiting confirmations");
   await transaction.wait();
 
